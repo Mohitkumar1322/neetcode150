c2b9d773d56b7b6947e8d63ee3076d6ac1579e91
Mohit kumar <124328700+Mohitkumar1322@users.noreply.github.com>
2025-08-26 15:37:08 +0530

    Add Find_minimum_in_rotated_array.cpp

---
+++ Find_minimum_in_rotated_array.cpp
@@ -0,0 +1,11 @@
+int n = nums.size();
+        int left=0,right=n-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]<nums[right]){
+                right=mid;
+            }else{
+                left=mid+1;
+            }
+        }
+        return nums[left];
