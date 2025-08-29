804094cbd63d091716218e65bf74f705a26514f4
Mohit kumar <124328700+Mohitkumar1322@users.noreply.github.com>
2025-08-29 12:03:12 +0530

    Add permutation.cpp

---
+++ permutation.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void generate(vector<int>& nums,vector<vector<int>>& ans,int index){
+        if(index==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=index;i<nums.size();i++){
+            swap(nums[index],nums[i]);
+            generate(nums,ans,index+1);
+            swap(nums[index],nums[i]);
+        }
+
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+       
+        generate(nums,ans,0);
+        return ans;
+        
+    }
+};
